1e2995adf856942f0c9145ea7e1f2c9dd9097753
Vladimir de la Cruz <vladimirdlc@gmail.com>
2014-12-07 18:05:14 -0430

    Hook can now hook to colliders

--- LD31_Snowman/Assets/Scripts/Gameplay/Powerups/Hook.cs
+++ LD31_Snowman/Assets/Scripts/Gameplay/Powerups/Hook.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Hook : MonoBehaviour {
     public float force = 400;
+    public float playerPushForce = 40000;
     public float retractForce = 3;
     public CircleCollider2D controlRadius;
     private bool isRetracting;
@@ -10,6 +11,8 @@ public class Hook : MonoBehaviour {
     public GameObject hookGuide;
     public float currentRetractTime;
     public float maxRetractTime = 1;
+    public GameObject player;
+    public bool isFlying;
 
 	// Update is called once per frame
 	void Update () {
@@ -19,14 +22,16 @@ public class Hook : MonoBehaviour {
             {
                 rigidbody2D.AddForce(transform.up * force);
                 currentRetractTime = 0;
-                //origin = transform.position;
-                ////originQuaternion = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+                isFlying = true;
+                collider2D.enabled = true;
             }
 
             if (!controlRadius.bounds.Contains(transform.position))
             {
                 rigidbody2D.velocity = Vector2.zero;
+                isFlying = false;
                 isRetracting = true;
+                collider2D.enabled = false;
             }
         }
         else retractHook();
@@ -53,5 +58,17 @@ public class Hook : MonoBehaviour {
     {
         transform.position = hookGuide.transform.position;
         transform.rotation = hookGuide.transform.rotation;
+    }
+
+    /// <summary>
+    /// Hook has collided with something
+    /// </summary>
+    /// <param name="coll">Other object</param>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        rigidbody2D.velocity = Vector3.zero;
+        collider2D.enabled = false;
+        player.rigidbody2D.AddForce(transform.up * playerPushForce);
+        isRetracting = true;
     }
 }
