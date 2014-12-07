f9dcc887f8f1b512f510164449daf1bc408e6953
Vladimir de la Cruz <vladimirdlc@gmail.com>
2014-12-07 13:45:07 -0430

    Added Hook basic rotation with pivot as prefab

---
+++ LD31_Snowman/Assets/Scripts/UI/MobileControls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MobileControls : MonoBehaviour {
+    public float speed = 4.0f;
+    public float smoothSteer = 4.0f;
+    public bool isUsingTouch;
+
+    void Update()
+    {
+        lookAtTouch(transform);
+    }
+        
+
+    public void lookAtTouch(Transform caller)
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        //transform.LookAt(new Vector3(mousePosition.x, mousePosition.y, transform.position.x));
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+    }
+
+}
+    
---
+++ LD31_Snowman/Assets/Scripts/UI/RotZClamper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotZClamper : MonoBehaviour {
+    public float minZClamp = 135.0f;
+    public float maxZClamp = 225.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 touchPosition = Input.mousePosition;
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (Vector3.Distance(transform.localPosition, touch.position)
+                < Vector3.Distance(transform.localPosition, touchPosition))
+                touchPosition = touch.position;
+        }
+
+        float curZ = transform.rotation.eulerAngles.z;
+        if (curZ > minZClamp && curZ < maxZClamp)
+            if (touchPosition.x < transform.position.x)
+            {
+                transform.rotation =
+                    Quaternion.Euler(transform.rotation.eulerAngles.x, 
+                    transform.rotation.eulerAngles.y, minZClamp);
+            }
+            else
+            {
+                transform.rotation =
+                    Quaternion.Euler(transform.rotation.eulerAngles.x,
+                    transform.rotation.eulerAngles.y, maxZClamp);
+            }
+	}
+}
