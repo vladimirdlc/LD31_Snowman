ab469ee6d10bb571ab5f08845163fa14c2dd3b69
Dincu <dgmarcellab@gmail.com>
2014-12-07 16:27:31 -0430

    Better aim
    Hand aims better towards the mouse cursor

---
+++ LD31_Snowman/Assets/Scripts/UI/GameObjectFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectFollow : MonoBehaviour {
+	public GameObject target;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.transform.position;
+	}
+}
--- LD31_Snowman/Assets/Scripts/UI/MobileControls.cs
+++ LD31_Snowman/Assets/Scripts/UI/MobileControls.cs
@@ -3,9 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class MobileControls : MonoBehaviour {
-    public float speed = 4.0f;
-    public float smoothSteer = 4.0f;
-    public bool isUsingTouch;
+    //public float speed = 4.0f;
+    //public float smoothSteer = 4.0f;
+    //public bool isUsingTouch;
 
     void Update()
     {
@@ -19,7 +19,7 @@ public class MobileControls : MonoBehaviour {
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
         //transform.LookAt(new Vector3(mousePosition.x, mousePosition.y, transform.position.x));
-        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+		Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
         transform.rotation = rot;
         transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
     }
