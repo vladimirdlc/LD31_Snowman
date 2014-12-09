5dc6a0290107e6f66e3f931af150ab7327fead53
Vladimir de la Cruz <vladimirdlc@gmail.com>
2014-12-08 19:39:04 -0430

    Added Red Hat with broader range functionality

---
+++ LD31_Snowman/Assets/Scripts/Gameplay/Powerups/FreedomGift.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreedomGift : MonoBehaviour {
+    public Sprite giftHat;
+    public SpriteRenderer hatRenderer;
+    public GameObject range;
+    public float rangeMultiplier = 10;
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            hatRenderer.sprite = giftHat;
+            range.transform.localScale = range.transform.localScale * rangeMultiplier;
+            Destroy(gameObject);
+        }
+            
+
+    }
+}
